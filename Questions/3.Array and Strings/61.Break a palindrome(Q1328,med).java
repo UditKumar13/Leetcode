a29e8377c6445278fb3143d52a43a3cb4159f4d1
Udit kumar <uditkumar2401@gmail.com>
2022-10-12 15:00:51 +0530

    lets start again

---
+++ Questions/3.Array and Strings/61.Break a palindrome(Q1328,med).java
@@ -0,0 +1,16 @@
+class Solution {
+      public String breakPalindrome(String palindrome) {
+          char [] s = palindrome.toCharArray();
+          int n = s.length;
+          for(int i=0;i<n/2;i++){
+              if(s[i] != 'a'){
+                  s[i] = 'a';
+                  return  String.valueOf(s);
+              }
+          }
+          
+          s[n-1] = 'b';
+          return (n<=1) ?  "" : String.valueOf(s) ;
+         
+    }
+}
