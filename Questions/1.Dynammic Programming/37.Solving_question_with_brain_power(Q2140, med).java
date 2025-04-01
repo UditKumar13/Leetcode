5dbfd575e046a326035a19f14f4b9d7fdd311624
uditkumar13 <udit21148@gmail.com>
2025-04-01 20:26:16 +0530

    updated code

--- Questions/1.Dynammic Programming/37.Solving_question_with_brain_power(Q2140, med).java
+++ Questions/1.Dynammic Programming/37.Solving_question_with_brain_power(Q2140, med).java
@@ -4,16 +4,16 @@ class Solution {
     public long mostPoints(int[][] questions) {
         long[] dp = new long[questions.length];
         for(int i = questions.length-1; i >= 0; i--){
-            int nextIndex = i + questions[i][1] + 1;
+            int nextIndex = i + questions[i][1] + 1; // nextIndex is the index of the next question that can be solved after solving the current question
             if(nextIndex < questions.length){
-                dp[i] = dp[nextIndex] + questions[i][0];
+                dp[i] = dp[nextIndex] + questions[i][0]; // if there are more questions to solve, take the maximum of the current question's points and the next question's points
             }else{
-                dp[i] = questions[i][0];
+                dp[i] = questions[i][0]; // if there are no more questions to solve, just take the points from the current question
             }
             if(i < questions.length-1){
-                dp[i] = Math.max(dp[i+1], dp[i]);
+                dp[i] = Math.max(dp[i+1], dp[i]); // take the maximum of the current question's points and the next question's points
             }
         }
-        return dp[0];
+        return dp[0]; // max result is stored in dp[0].
     }
 }
